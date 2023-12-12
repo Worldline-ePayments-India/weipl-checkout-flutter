dbfb7ca65be30ff02179ee992a228472c0653102
Praveen.Yadav <praveen.yadav@worldline.com>
2023-12-12 22:20:00 +0530

    FIX: Additional exception handling implemented

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.2
+
+- Additional exception handling fix applied for Android platform
+
 ## 1.0.1
 
 - Response handling fix applied for Android platform
--- android/src/main/java/com/worldline/in/weipl_checkout_flutter/WeiplCheckoutFlutterPlugin.java
+++ android/src/main/java/com/worldline/in/weipl_checkout_flutter/WeiplCheckoutFlutterPlugin.java
@@ -17,6 +17,7 @@ import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.PluginRegistry.ActivityResultListener;
 
 import com.weipl.checkout.WLCheckoutActivity;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,170 +27,191 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-/** WeiplCheckoutFlutterPlugin */
+
+/**
+ * WeiplCheckoutFlutterPlugin
+ */
 public class WeiplCheckoutFlutterPlugin implements FlutterPlugin, MethodCallHandler, WLCheckoutActivity.PaymentResponseListener, ActivityAware {
-  // The MethodChannel that will the communication between Flutter and native Android
-  // This local reference serves to register the plugin with the Flutter Engine and unregister it
-  // when the Flutter Engine is detached from the Activity
+    // The MethodChannel that will the communication between Flutter and native Android
+    // This local reference serves to register the plugin with the Flutter Engine and unregister it
+    // when the Flutter Engine is detached from the Activity
 
-  private static final String PLUGIN_NAME = "weipl_checkout_flutter";
-  private MethodChannel channel;
+    private static final String PLUGIN_NAME = "weipl_checkout_flutter";
+    private static final String TAG = "WeiplCheckout";
+    private static MethodChannel channel;
 
-  private Context context;
-  private static Activity activity;
+    private static Activity activity;
 
-  private static Result callback;
+    private static Result callback;
 
-  public WeiplCheckoutFlutterPlugin() {
+    public WeiplCheckoutFlutterPlugin() {
 
-  }
-
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), PLUGIN_NAME);
-    activity = registrar.activity();
-    channel.setMethodCallHandler(new WeiplCheckoutFlutterPlugin());
-  }
-
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), PLUGIN_NAME);
-    channel.setMethodCallHandler(this);
-    context = flutterPluginBinding.getApplicationContext();
-    WLCheckoutActivity.setPaymentResponseListener(this);
-    WLCheckoutActivity.preloadData(context);
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    this.callback = result;
-    if (call.method.equals("open")) {
-      this.open((Map<String, Object>) call.arguments);
-    } else if (call.method.equals("upiIntentAppsList")) {
-      this.upiIntentAppsList();
-    } else {
-      result.notImplemented();
-    }
-  }
-
-  private void open(Map<String, Object> args) {
-    if (args == null) {
-      this.callback.success("Expected checkout initialisation options");
-      return;
     }
 
-    try {
-      JSONObject convertedObject = new JSONObject(args);
-      WLCheckoutActivity.open(activity, convertedObject);
-    } catch(Exception e) {
-      Log.d("Error", e.getLocalizedMessage());
-      this.callback.success("Something went wrong " + e);
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), PLUGIN_NAME);
+        activity = registrar.activity();
+        channel.setMethodCallHandler(new WeiplCheckoutFlutterPlugin());
     }
-  }
 
-  private void upiIntentAppsList() {
-    JSONArray upiIntentResponse = WLCheckoutActivity.getUPIResponse(activity);
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), PLUGIN_NAME);
+        channel.setMethodCallHandler(this);
 
-    if (upiIntentResponse == null) {
-      this.callback.success("No response received!");
-    } else {
-      List<Object> retList = new ArrayList<Object>();
-      if(upiIntentResponse != null) {
+        Context context;
+        context = flutterPluginBinding.getApplicationContext();
+        WLCheckoutActivity.setPaymentResponseListener(this);
+        WLCheckoutActivity.preloadData(context);
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        this.callback = result;
         try {
-          retList = toList(upiIntentResponse);
-        } catch (JSONException e) {
-          throw new RuntimeException(e);
+            if (call.method.equals("open")) {
+                this.open((Map < String, Object > ) call.arguments);
+            } else if (call.method.equals("upiIntentAppsList")) {
+                this.upiIntentAppsList();
+            } else {
+                result.notImplemented();
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "Error occurred in onMethodCall() :\n" + e.getLocalizedMessage());
         }
-      }
-
-      this.callback.success(retList);
-    }
-  }
-
-  public static Map<String, Object> toMap(JSONObject object) throws JSONException {
-    Map<String, Object> map = new HashMap<String, Object>();
-
-    Iterator<String> keysItr = object.keys();
-    while(keysItr.hasNext()) {
-      String key = keysItr.next();
-      Object value = object.get(key);
-
-      if(value instanceof JSONArray) {
-        value = toList((JSONArray) value);
-      }
-      else if(value instanceof JSONObject) {
-        value = toMap((JSONObject) value);
-      }
-      map.put(key, value);
-    }
-    return map;
-  }
-
-  public static List<Object> toList(JSONArray array) throws JSONException {
-    List<Object> list = new ArrayList<Object>();
-    for(int i = 0; i < array.length(); i++) {
-      Object value = array.get(i);
-      if(value instanceof JSONArray) {
-        value = toList((JSONArray) value);
-      }
-      else if(value instanceof JSONObject) {
-        value = toMap((JSONObject) value);
-      }
-      list.add(value);
-    }
-    return list;
-  }
-
-  @Override
-  public void wlCheckoutPaymentResponse(JSONObject jsonObject) {
-    Map<String, Object> retMap = new HashMap<String, Object>();
-    if(jsonObject != null) {
-      try {
-        retMap = toMap(jsonObject);
-      } catch (JSONException e) {
-        throw new RuntimeException(e);
-      }
     }
 
-    this.callback.success(retMap);
-  }
+    private void open(Map < String, Object > args) {
+        if (args == null) {
+            this.callback.success("Expected checkout initialisation options");
+            return;
+        }
 
-  @Override
-  public void wlCheckoutPaymentError(JSONObject jsonObject) {
-    Map<String, Object> retMap = new HashMap<String, Object>();
-    if(jsonObject != null) {
-      try {
-        retMap = toMap(jsonObject);
-      } catch (JSONException e) {
-        throw new RuntimeException(e);
-      }
+        try {
+            JSONObject convertedObject = new JSONObject(args);
+            WLCheckoutActivity.open(activity, convertedObject);
+        } catch (Exception e) {
+            Log.d(TAG, e.getLocalizedMessage());
+            throw new RuntimeException(e);
+        }
     }
 
-    this.callback.success(retMap);
-  }
+    private void upiIntentAppsList() {
+        JSONArray upiIntentResponse = WLCheckoutActivity.getUPIResponse(activity);
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+        if (upiIntentResponse == null) {
+            if (this != null && this.callback != null) {
+                this.callback.success("No response received!");
+            }
+            Log.d(TAG, "No response received!");
+        } else {
+            List < Object > retList = new ArrayList < Object > ();
+            if (upiIntentResponse != null) {
+                try {
+                    retList = toList(upiIntentResponse);
+                    this.callback.success(retList);
+                } catch (Exception e) {
+                    Log.d(TAG, "Got error in upiIntentAppsList()");
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 
-  @Override
-  public void onDetachedFromActivity() {
+    public static Map < String, Object > toMap(JSONObject object) throws JSONException {
+        Map < String, Object > map = new HashMap < String, Object > ();
 
-  }
+        Iterator < String > keysItr = object.keys();
+        while (keysItr.hasNext()) {
+            String key = keysItr.next();
+            Object value = object.get(key);
 
-  @Override
-  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
-    activity = binding.getActivity();
-  }
+            if (value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            } else if (value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            map.put(key, value);
+        }
+        return map;
+    }
 
-  @Override
-  public void onAttachedToActivity(ActivityPluginBinding binding) {
-    activity = binding.getActivity();
-  }
+    public static List < Object > toList(JSONArray array) throws JSONException {
+        List < Object > list = new ArrayList < Object > ();
+        for (int i = 0; i < array.length(); i++) {
+            Object value = array.get(i);
+            if (value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            } else if (value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            list.add(value);
+        }
+        return list;
+    }
 
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {
+    @Override
+    public void wlCheckoutPaymentResponse(JSONObject jsonObject) {
+        Map < String, Object > retMap = new HashMap < String, Object > ();
+        if (jsonObject != null) {
+            try {
+                retMap = toMap(jsonObject);
+                this.callback.success(retMap);
+            } catch (Exception e) {
+                Log.d(TAG, "Got error in wlCheckoutPaymentResponse()");
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
-  }
+    @Override
+    public void wlCheckoutPaymentError(JSONObject jsonObject) {
+        Map < String, Object > retMap = new HashMap < String, Object > ();
+        if (jsonObject != null) {
+            try {
+                retMap = toMap(jsonObject);
+                this.callback.success(retMap);
+            } catch (Exception e) {
+                Log.d(TAG, "Got error in wlCheckoutPaymentError()");
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        try {
+            channel.setMethodCallHandler(null);
+        } catch (Exception e) {
+            Log.d(TAG, "Got error in onDetachedFromEngine()");
+        }
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+        try {
+            activity = binding.getActivity();
+        } catch (Exception e) {
+            Log.d(TAG, "Got error in onReattachedToActivityForConfigChanges()");
+        }
+    }
+
+    @Override
+    public void onAttachedToActivity(ActivityPluginBinding binding) {
+        try {
+            activity = binding.getActivity();
+        } catch (Exception e) {
+            Log.d(TAG, "Got error in onAttachedToActivity()");
+        }
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+
+    }
 }
